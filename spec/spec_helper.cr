dded4374911fd7f05586653c3c8ffc75216b410f
Anton Maminov <anton.linux@gmail.com>
2021-02-16 14:32:34 +0200

    use pg

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -8,15 +8,16 @@ class ::Crypto::Bcrypt::Password
 end
 
 def initdb
-  system("echo \"DROP DATABASE IF EXISTS clear_spec;\" | psql -U postgres 2>&1 1>/dev/null")
-  system("echo \"CREATE DATABASE clear_spec;\" | psql -U postgres 2>&1 1>/dev/null")
+  pg.exec("DROP DATABASE IF EXISTS clear_spec;")
+  pg.exec("CREATE DATABASE clear_spec;")
 
-  system("echo \"DROP DATABASE IF EXISTS clear_secondary_spec;\" | psql -U postgres 2>&1 1>/dev/null")
-  system("echo \"CREATE DATABASE clear_secondary_spec;\" | psql -U postgres 2>&1 1>/dev/null")
-  system("echo \"CREATE TABLE models_post_stats (id serial PRIMARY KEY, post_id INTEGER);\" | psql -U postgres clear_secondary_spec 2>&1 1>/dev/null")
+  pg.exec("DROP DATABASE IF EXISTS clear_secondary_spec;")
+  pg.exec("CREATE DATABASE clear_secondary_spec;")
+  pg.exec("CREATE TABLE models_post_stats (id serial PRIMARY KEY, post_id INTEGER);")
 
-  Clear::SQL.init("postgres://postgres@localhost/clear_spec", connection_pool_size: 5)
-  Clear::SQL.init("secondary", "postgres://postgres@localhost/clear_secondary_spec", connection_pool_size: 5)
+
+  Clear::SQL.init("postgres://postgres:postgres@localhost/clear_spec", connection_pool_size: 5)
+  Clear::SQL.init("secondary", "postgres://postgres:postgres@localhost/clear_secondary_spec", connection_pool_size: 5)
 
   {% if flag?(:quiet) %} Log.setup(:error) {% else %} Log.setup(:debug) {% end %}
 end
@@ -31,5 +32,15 @@ def temporary(&block)
     Clear::SQL.rollback
   end
 end
+
+def pg
+  postgres_user = ENV["POSTGRES_USER"]? || "postgres"
+  postgres_password = ENV["POSTGRES_PASSWORD"]? || ""
+  postgres_host = ENV["POSTGRES_HOST"]? || "localhost"
+  postgres_db = ENV["POSTGRES_DB"]? || "postgres"
+
+  DB.open("postgres://#{postgres_user}:#{postgres_password}@#{postgres_host}/#{postgres_db}")
+end
+
 
 initdb
